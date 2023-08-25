a917d0710bbcb0e94aeb9c96fe34bdf285918610
dalton bigirimana <daltonbigirimana5@gmail.com>
2023-08-25 16:58:08 +0200

    python list

--- pythoncourses.py
+++ pythoncourses.py
@@ -2,3 +2,76 @@
 x = 5
 y = "John"
 print(x)
+
+# list
+mylist = ["dalton", "banana", "cherry"]
+print(mylist)
+print(len(mylist))
+mylist.append("azubi africa")
+print(mylist)
+mylist.clear()
+print(mylist)
+# List slicing in Python
+
+my_list = ['p','r','o','g','r','a','m','i','z']
+
+# items from index 2 to index 4
+print(my_list[2:5])
+
+# items from index 5 to end
+print(my_list[5:])
+
+# items beginning to end
+print(my_list[:])
+numbers = [21, 34, 54, 12]
+
+print("Before Append:", numbers)
+
+# using append method
+numbers.append(32)
+
+print("After Append:", numbers)
+
+numbers = [1, 3, 5]
+
+even_numbers = [4, 6, 8]
+
+# add elements of even_numbers to the numbers list
+numbers.extend(even_numbers)
+
+print("List after append:", numbers) 
+
+numbers = [10, 30, 40]
+
+# insert an element at index 1 (second position)
+numbers.insert(1, 20)
+
+print(numbers) # [10, 20, 30, 40]
+
+languages = ['Python', 'Swift', 'C++']
+
+# changing the third item to 'C'
+languages[2] = 'C'
+
+print(languages)  # ['Python', 'Swift', 'C']
+
+languages = ['Python', 'Swift', 'C++', 'C', 'Java', 'Rust', 'R']
+
+# deleting the second item
+del languages[1]
+print(languages) # ['Python', 'C++', 'C', 'Java', 'Rust', 'R']
+
+# deleting the last item
+del languages[-1]
+print(languages) # ['Python', 'C++', 'C', 'Java', 'Rust']
+
+# delete the first two items
+del languages[0 : 2]  # ['C', 'Java', 'Rust']
+print(languages)
+
+languages = ['Python', 'Swift', 'C++', 'C', 'Java', 'Rust', 'R']
+
+# remove 'Python' from the list
+languages.remove('Python')
+
+print(languages) # ['Swift', 'C++', 'C', 'Java', 'Rust', 'R']
